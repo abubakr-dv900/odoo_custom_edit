c92b970fb023c8dce6f7ac9a5b0e2c7f2e010776
mmasaad-ops <zakeimm99@gmail.com>
2026-08-08 11:25:15 +0200

    this model use to show due balance in pos in mobile which odoo by default hide it

---
+++ pos_partner_list/__manifest__.py
@@ -0,0 +1,21 @@
+{
+"name":"pos partner due balance list in mobile",
+"verion":"1.0.0"
+"author":"abubakr",
+"depend":["point_of_sale"],
+"descrption":"this odoo module add partner list in mobile device"
+"category": "point_of_sale"
+
+"assets":{
+    "point_of_Sale._assets_pos":[
+        "pos_partner_list/static/src/partnerline.xml",
+    ]
+}
+
+"installable":True,
+"application":True
+
+
+
+
+}
