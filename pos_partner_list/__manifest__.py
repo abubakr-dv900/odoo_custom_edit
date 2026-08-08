c4636db7af91faf5bce12e413e29453cf00972ac
mmasaad-ops <zakeimm99@gmail.com>
2026-08-08 11:26:45 +0200

    fix syntax

--- pos_partner_list/__manifest__.py
+++ pos_partner_list/__manifest__.py
@@ -2,18 +2,17 @@
 "name":"pos partner due balance list in mobile",
 "verion":"1.0.0"
 "author":"abubakr",
-"depend":["point_of_sale"],
-"descrption":"this odoo module add partner list in mobile device"
+"depends":["point_of_sale"],
+"description":"this odoo module add partner list in mobile device"
 "category": "point_of_sale"
 
 "assets":{
-    "point_of_Sale._assets_pos":[
+    "point_of_sale._assets_pos":[
         "pos_partner_list/static/src/partnerline.xml",
     ]
 }
 
 "installable":True,
-"application":True
 
 
 
