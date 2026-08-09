df9657bada47cf55c429ae847335495ccfc3c170
mmasaad-ops <zakeimm99@gmail.com>
2026-08-09 17:44:25 +0200

    report redesign

---
+++ custom_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+'name':'custom report',
+'version':'1.1.0',
+'author':'abubakr',
+'category':'technecal',
+'depends':['sale'],
+'data':[
+    "report/sale_order.xml",
+],
+'installable':True,
+'application':True,
+
+}
